911b3b3db5a6a3cc743e06c5c9569333d29df70e
kennycodes <kennyspeaks@gmail.com>
2019-08-01 21:31:55 +1200

    Modified existing tables and added new ones

--- dbMessyItUp/Release/dbRelease_0.2.sql
+++ dbMessyItUp/Release/dbRelease_0.2.sql
@@ -4,66 +4,66 @@ CREATE SCHEMA miu;
 
 -- Create Customer Gender Table
 
-CREATE TABLE IF NOT EXISTS miu.customer_gender(
-   gender_id serial PRIMARY KEY,
-   gender_name VARCHAR (10)
+CREATE TABLE IF NOT EXISTS miu.genders(
+   id serial PRIMARY KEY,
+   name VARCHAR (10)
 );
 
 -- Create Customer Account table
 
-CREATE TABLE IF NOT EXISTS miu.customer_account(
-   account_id serial PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS miu.accounts(
+   id serial PRIMARY KEY,
    first_name VARCHAR (50) NOT NULL,
    last_name VARCHAR (50),
-   account_password VARCHAR (50) NOT NULL,
-   email VARCHAR (355) UNIQUE NOT NULL,
+   password VARCHAR (50) NOT NULL,
+   email_address VARCHAR (355) UNIQUE NOT NULL,
    phone_number VARCHAR (50),
    date_of_birth DATE,
    gender_id INT, 
-   created_on TIMESTAMP NOT NULL,
-   verified_on TIMESTAMP,
-   last_login TIMESTAMP NOT NULL
+   created_at TIMESTAMP NOT NULL,
+   verified_at TIMESTAMP,
+   last_login_at TIMESTAMP NOT NULL,
+   last_modified_at TIMESTAMP NOT NULL
 );
 
 
-ALTER TABLE miu.customer_account ADD CONSTRAINT fkey_customer_account_gender_id FOREIGN KEY (gender_id)
-      REFERENCES miu.customer_gender (gender_id) MATCH SIMPLE
+ALTER TABLE miu.accounts ADD CONSTRAINT fkey_customer_account_gender_id FOREIGN KEY (gender_id)
+      REFERENCES miu.genders (gender_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 -- Create Customer Device table
 
-CREATE TABLE miu.customer_device(
-   device_id serial PRIMARY KEY,
-   device_serial_number VARCHAR (50),
-   device_name VARCHAR (50),
-   device_os VARCHAR (50)
+CREATE TABLE miu.devices(
+   id SERIAL PRIMARY KEY,
+   serial_number VARCHAR (50),
+   name VARCHAR (50),
+   operating_system VARCHAR (50)
 );
 
 -- Create Account Device table
 
-CREATE TABLE miu.account_device(
+CREATE TABLE miu.account_devices(
    account_id INTEGER NOT NULL,
-   device_id INTEGER NOT NULL,
-   grant_date TIMESTAMP,
+   device_id INTEGER NOT NULL
   PRIMARY KEY (account_id, device_id),
   CONSTRAINT fkey_account_device_account_id FOREIGN KEY (account_id)
-      REFERENCES miu.customer_account (account_id) MATCH SIMPLE
+      REFERENCES miu.accounts (account_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT fkey_account_device_device_id FOREIGN KEY (device_id)
-      REFERENCES miu.customer_device (device_id) MATCH SIMPLE
+      REFERENCES miu.devices (device_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 -- Create Customer Location table
 
-CREATE TABLE miu.customer_location(
-   location_id serial PRIMARY KEY,
+CREATE TABLE miu.locations(
+   id SERIAL PRIMARY KEY,
    account_id INTEGER NOT NULL,
    city VARCHAR (50),
    region VARCHAR (50),
    country VARCHAR (355) NOT NULL,
    ip_address VARCHAR (50),
-   recorded_on TIMESTAMP NOT NULL,
+   recorded_at TIMESTAMP NOT NULL,
    CONSTRAINT fkey_customer_loc_account_id FOREIGN KEY (account_id)
       REFERENCES miu.customer_account (account_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
@@ -71,11 +71,29 @@ CREATE TABLE miu.customer_location(
 
 -- Create Generic Config Table
 
-CREATE TABLE miu.generic_config(
-   config_id SERIAL PRIMARY KEY,
-   config_name VARCHAR(100) NOT NULL,
-   config_type VARCHAR(50),
-   config_value VARCHAR(5000)
+CREATE TABLE miu.configs(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(100) NOT NULL,
+   type VARCHAR(50),
+   value VARCHAR(5000)
 );
 
+-- Create Pins Table
+
+CREATE TABLE miu.pins(
+   email_address VARCHAR(355) PRIMARY KEY,
+   pin INTEGER NOT NULL,
+   purpose VARCHAR(50),
+   expired_at TIMESTAMP 
+);
+
+-- Create Revoked Tokens Table
+
+CREATE TABLE miu.revoked_tokens(
+   jti uuid PRIMARY KEY,
+   token VARCHAR(700) NOT NULL,
+   account_id INTEGER,
+   expired_at TIMESTAMP,
+   revoked_at TIMESTAMP
+);
 
